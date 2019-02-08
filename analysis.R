83ca8feafdcd35d2aab92bafb478d9763fdeec3e
Eddie Yeuan <eddieyeuan@yahoo.com>
2019-02-08 10:28:42 -0800

    exercise

--- analysis.R
+++ analysis.R
@@ -1,17 +1,29 @@
 # Analysis script: compute values and create graphics of interest
 library("dplyr")
+install.packages("ggplot2")
 library("ggplot2")
+install.packages("lubridate")
 library("lubridate")
 library("tidyr")
+install.packages("ggmap")
 library("ggmap")
 
 # Load in your data
+data <- read.csv("data/Eviction_Notices.csv", stringsAsFactors = FALSE)
 
 # Compute some values of interest and store them in variables for the report
 
+
 # How many evictions were there?
+num <- nrow(data)
 
 # Create a table (data frame) of evictions by zip code (sort descending)
+zip <- data %>% 
+  group_by(Eviction.Notice.Source.Zipcode) %>% 
+  count() %>%
+  arrange(-n) %>%
+  ungroup() %>%
+  top_n(10, wt = n)
 
 # Create a plot of the number of evictions each month in the dataset
 
